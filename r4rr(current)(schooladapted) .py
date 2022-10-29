13992131aed8b5ced2dc17f40142a297384a761e
Ivan Gudkov <45141610+justanuman@users.noreply.github.com>
2022-10-29 23:08:50 +0300

    hiding a leak
    This changes nothing since this key is expired, but just in case.

--- r4rr(current)(schooladapted) .py
+++ r4rr(current)(schooladapted) .py
@@ -6,7 +6,7 @@ file = open("id"+'.txt',"w")
 id1= ["stepab","seregam","vadim","serega"]
 massid = ["02e66b12-b760-4320-925e-da78489a38ea","3951687f-9b54-4093-ac1a-4d5ec8f49e70","42b0407a-b27c-480b-b20a-f619888b8594","a0ca509e-a0c3-4334-891f-1566ad7097ba"]
 nam = []
-SUBSCRIPTION_KEY = '601843bed46445f6a879303f84e39e73'   
+SUBSCRIPTION_KEY = 'this key is invalid'   
 CF.Key.set(SUBSCRIPTION_KEY)
 BASE_URL = 'https://northeurope.api.cognitive.microsoft.com/face/v1.0'  
 CF.BaseUrl.set(BASE_URL)
